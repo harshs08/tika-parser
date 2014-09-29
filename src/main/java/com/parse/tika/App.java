7e9bbbdff9a399f606ef0604b93f2146d62a52cc
Harsh Singh <harshsin@usc.edu>
2014-09-28 18:20:31 -0700

    added the time counter

--- src/main/java/com/parse/tika/App.java
+++ src/main/java/com/parse/tika/App.java
@@ -6,6 +6,9 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import org.apache.tika.exception.TikaException;
 import org.apache.tika.metadata.Metadata;
@@ -25,19 +28,23 @@ public class App {
 
 		File f = new File(
 				"/Users/harshsingh/Documents/Codes/cs572/baron.pagemewhen.com/~chris/employment/subset/computrabajo-ar-20121106.tsv");
-
+		
+		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss.SSS");
+		Calendar cal = Calendar.getInstance();
+		System.out.println("Parse start time: "+dateFormat.format(cal.getTime()));
 		parser.parse(new FileInputStream(f), h, metadata, new ParseContext());
-
+		
 		//System.out.println(h.toString());
 
 		String fileName = "/Users/harshsingh/Desktop/run2.html";
 		
-
 		PrintWriter writer = null;
 		try {
 			writer = new PrintWriter(fileName, "UTF-8");
-
 			writer.println(h.toString());
+			DateFormat dateFormat2 = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss.SSS");
+			Calendar cal2 = Calendar.getInstance();
+			System.out.println("Parse end time: "+dateFormat2.format(cal2.getTime()));
 		}
 
 		catch (Exception e) {
